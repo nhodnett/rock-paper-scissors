3a567666f2953c21b29a9baa51142874fb84db07
Nathan Hodnett <nathanhodnett@gmail.com>
2022-03-01 23:38:31 -0700

    Add initial js  html and css

---
+++ main.js
@@ -0,0 +1,21 @@
+// A main.js file that contains all DOM related JavaScript
+
+// In a game like Rock Paper Scissors, it is tempting to manipulate the DOM first.
+// Remember that the game logic exists exclusively in the data model.
+// The DOM simply reflects/displays that data model.
+
+// Query Selectors
+
+
+// Event Listeners
+
+
+// Global Variables
+var game = new Game;
+
+// Functions
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+};
+
+// What to do first?
---
+++ player.js
@@ -0,0 +1,17 @@
+// A player.js file that contains a Player class.
+// Player methods must include, but are not limited to:
+// Constructor - properties should include: name (ex: 'Human'),
+// token (ex: ':woman::skin-tone-2:'), wins (ex: 0)
+// takeTurn
+
+class Player {
+  constructor(name, token, wins) {
+    this.name = name;
+    this.token = token;
+    this.wins = wins;
+  }
+};
+
+takeTurn() {
+
+}

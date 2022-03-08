50cba493ee3d24d68baf374094247abf762b6ca9
Nathan Hodnett <nathanhodnett@gmail.com>
2022-03-07 20:31:56 -0700

    Add data model logic to player.js and refactor html

--- player.js
+++ player.js
@@ -1,26 +1,23 @@
-// A player.js file that contains a Player class.
-// Player methods must include, but are not limited to:
-// Constructor - properties should include: name (ex: 'Human'),
-// token (ex: ':woman::skin-tone-2:'), wins (ex: 0)
-// takeTurn
-
 class Player {
   constructor(name, token) {
     this.name = name;
-    this.token = token; // emoji...
+    this.token = token;
     this.wins = 0;
-    // this.choice = ""; // buttons... Am I doing anything with this?
-    // extension - reset wins...
+    this.choice = "";
   }
+
+takeTurn() {
+  var classicWeapons = ["rock", "paper", "scissors"]
+  var hardWeapons = ["rock", "paper", "scissors", "lizard", "alien"]
+
+  if (this.name === "human") {
+    this.choice = event.target.id
+  };
+  if (this.name === "computer" && currentGame.gameType === "classic") {
+    this.choice = classicWeapons[getRandomIndex(classicWeapons)]
+  }
+  else if (this.name === "computer" && currentGame.gameType === "hard") {
+    this.choice = hardWeapons[getRandomIndex(hardWeapons)]
+  }
+}
 };
-
-//What is the Token for?
-
-//takeTurn() {
-// call it in main.js
-// conditional - if player name === human, get their click input, (event.target)
-// if this.name === computer, (getRamdomIndex)
-  // 2 arrays - 1 for classic, 1 for hard, (for computer)
-//}
-
-// resetWins()...
